7165b649c19281b8783b2addae720c895b865580
John-Osiko <johnmaxosiko@gmail.com>
2020-11-07 15:09:40 +0300

    Created my database model classes

--- app/models.py
+++ app/models.py
@@ -1,3 +1,71 @@
+import uuid
+
 from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils import timezone
+
+from .managers import CustomUserManager
 
 # Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    
+    # These fields tie to the roles!
+    ADMIN = 1
+    CUSTOMER = 2
+
+    ROLE_CHOICES = (
+        (ADMIN, 'Admin'),
+        (CUSTOMER, 'Customer'),
+    )
+    uid = models.UUIDField(unique=True, editable=False, default=uuid.uuid4, verbose_name='Public identifier')
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, blank=True, null=True, default=3)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
+    created_date = models.DateTimeField(default=timezone.now)
+    modified_date = models.DateTimeField(default=timezone.now)
+    created_by = models.EmailField()
+    modified_by = models.EmailField()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+
+
+class Product(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=50)
+    product_price = models.IntegerField()
+    product_description = models.CharField(max_length=250)
+    product_category = models.CharField(max_length=50)
+
+    def save_product(self):
+        self.save()
+
+    def delete_product(self):
+        self.delete()
+
+    @classmethod
+    def search_by_category(cls, product_category):
+        products = cls.objects.filter(category__name__icontains=product_category)
+        return products
+    class Meta:
+        db_table = 'products'
+
+class ShopProduct(models.Model):
+    prod_name = models.CharField(max_length=40)
+    prod_description = models.TextField()
+    prod_price = models.DecimalField(decimal_places=2, max_digits=20)
